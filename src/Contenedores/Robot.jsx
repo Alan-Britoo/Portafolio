0c4c379902c0e0ba62b956078e6db26d2ad0ddcb
Alan-Britoo <rotcarrillo_8@hotmail.com>
2024-03-24 03:59:57 -0500

    robot

--- src/Contenedores/Robot.jsx
+++ src/Contenedores/Robot.jsx
@@ -15,24 +15,32 @@ function Robot() {
   };
 
   return (
-    <div className="fixed bottom-14 right-9 z-10">
+    <div className="fixed bottom-14 right-9 z-10 ">
       {expanded && (
-        <div className="mt-2 rounded-lg shadow-md p-1 flex flex-col gap-3 transition-all duration-1000">
-          <a
+        <div className="mt-2 rounded-lg shadow-md p-1 flex flex-col gap-3 transition-all duration-1000 ">
+          {/* <a
             href="https://github.com/Alan-Britoo"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-2"
           >
-            <img src={Git} alt="Correo" className="w-12 h-12" />
-          </a>
+            <img
+              src={Git}
+              alt="Correo"
+              className="w-12 h-12 max-sm:w-[34px]  max-sm:h-[34px]"
+            />
+          </a> */}
           <a
             href="https://www.linkedin.com/in/rotsenestefanel/"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-2"
+            className="flex items-center gap-1"
           >
-            <img src={Link} alt="Correo" className="w-12 h-12" />
+            <img
+              src={Link}
+              alt="Correo"
+              className="w-12 h-12 max-sm:w-[34px]  max-sm:h-[34px]"
+            />
           </a>
           <a
             href="https://wa.me/+573175200679"
@@ -40,24 +48,32 @@ function Robot() {
             rel="noopener noreferrer"
             className="flex items-center gap-2"
           >
-            <img src={Wp2} alt="WhatsApp" className="w-12 h-12" />
+            <img
+              src={Wp2}
+              alt="WhatsApp"
+              className="w-12 h-12 max-sm:w-[37px]  max-sm:h-[37px]"
+            />
           </a>
           <a
             href="mailto:rotcarrillo_8@hotmail.com"
             className="flex items-center gap-2 mb-2"
           >
-            <img src={Gm} alt="Correo" className="w-12 h-12" />
+            <img
+              src={Gm}
+              alt="Correo"
+              className="w-12 h-12 max-sm:w-[38px]  max-sm:h-[38px]"
+            />
           </a>
         </div>
       )}
       <div
-        className="w-14 h-14 bg-gradient-to-r from-pink-500 to-blue-600 rounded-full flex items-center justify-center cursor-pointer shadow-lg animate-pulse transform hover:scale-110 transition duration-1000"
+        className="w-14 h-14 bg-gradient-to-r from-pink-500 to-blue-600 rounded-full flex items-center justify-center cursor-pointer shadow-lg animate-pulse transform hover:scale-110 transition duration-1000 max-sm:w-[43px]  max-sm:h-[43px] "
         onClick={toggleOptions}
       >
         <img
           src={Yo}
           alt="Robot"
-          className={`w-[50px] h-[50px] rounded-full transition ${
+          className={`w-[50px] h-[50px] max-sm:w-[41px]  max-sm:h-[40px]  rounded-full transition ${
             expanded ? "rotate-180" : ""
           }`}
         />
